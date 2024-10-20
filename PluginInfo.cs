68ff6f41381b47eae16729b43b770a7e9f575a71
untitledbuilds <bponcebackup@gmail.com>
2024-10-20 14:56:02 -0700

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,8 @@
+    internal class PluginInfo
+    {
+        public const string GUID = "org.tex.gorillatag.textemplate";
+        public const string Name = "Tex BETA";
+        public const string Description = "Made by @untitledbuilds";
+        public const string Version = "1.8.0";
+    }
+}
